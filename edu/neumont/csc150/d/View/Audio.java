daa6462bf29bf015eeebf04011fe8071a7f8b0e3
Lawrence Douglas <LDouglas@student.neumont.edu>
2017-02-23 12:38:59 -0700

    Audio

---
+++ edu/neumont/csc150/d/View/Audio.java
@@ -0,0 +1,60 @@
+package edu.neumont.csc150.d.View;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
+//Class for all audio files
+public class Audio {
+	
+	//Main menu music
+	public void mainMenuMusic() {
+		URL url = Audio.class.getResource("Aftermath.wav");
+		AudioClip clip = Applet.newAudioClip(url);
+		clip.loop();
+	}
+	
+	//Classroom Music
+	public void roomMusic() {
+		URL url = Audio.class.getResource("ThunderDreams.wav");
+		AudioClip clip = Applet.newAudioClip(url);
+		clip.loop();
+	}
+	
+	//Commons music
+	public void commonsMusic() {
+		URL url = Audio.class.getResource("AwkwardMeeting.wav");
+		AudioClip clip = Applet.newAudioClip(url);
+		clip.loop();
+	}
+	
+	//Basement Music
+	public void basementMusic() {
+		URL url = Audio.class.getResource("NervousPiano.wav");
+		AudioClip clip = Applet.newAudioClip(url);
+		clip.loop();
+	}
+	
+	//Final boss battle music
+	public void bossBattleMusic() {
+		URL url = Audio.class.getResource("ControlledChaos.wav");
+		AudioClip clip = Applet.newAudioClip(url);
+		clip.loop();
+	}
+	
+	//Music during chase sequences
+	//May not even use
+	public void chaseMusic() {
+		
+	}
+	
+	//Sound for item pick up
+	public void itemPickUpSound() {
+		
+	}
+	
+	//May not use
+	public void walkingSound() {
+		
+	}
+}
